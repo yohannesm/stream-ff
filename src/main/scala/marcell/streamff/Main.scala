3b46c8ac2a850da377fe7ece29e65780a811d476
yohannesm <yohannes.marcell@gmail.com>
2019-04-16 22:08:40 -0500

    changed app to ioApp

--- src/main/scala/marcell/streamff/Main.scala
+++ src/main/scala/marcell/streamff/Main.scala
@@ -3,9 +3,10 @@ package marcell.streamff
 import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
 
-object Main extends App {
+object Main extends IOApp {
   //def run(args: List[String]) =
   // StreamffServer.stream[IO].compile.drain.as(ExitCode.Success)
-
-  println("initial commit")
+  def run(args: List[String]) = {
+    IO(println(s"end of program")).as(ExitCode.Success)
+  }
 }
